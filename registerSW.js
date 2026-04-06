de4173658d5efd4ba0970b84227b3538364cebb6
Sokkanathan K <Stark@MacBook-Pro.local>
2026-04-06 23:02:13 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Sky-Cafe/sw.js', { scope: '/Sky-Cafe/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
